1ddb94fcaea6fbf7396a30c1a49232d2d83888fd
lucianojamimbo <65134690+lucianojamimbo@users.noreply.github.com>
2021-07-04 16:56:15 +0100

    Core functionality complete
    The script can filter based on a keyword and/or a download attribute.
    .zip files are automatically unzipped.

--- CC0TextureDownloader.py
+++ CC0TextureDownloader.py
@@ -1,22 +1,22 @@
 import requests
 import csv
+			if i[2] == "ZIP":
+				print("Unzipping {0}_{1}.{2}".format(i[0],i[1],i[2]))
+				print(i[0]+'_'+i[1]+'.'+i[2])
+				with zipfile.ZipFile(i[0]+'_'+i[1]+'.'+i[2], 'r') as zip_ref:
+					zip_ref.extractall(i[0]+'_'+i[1])
+		except:
+			print("Failed to download {0}_{1} from {2}".format(i[0], i[1], i[4]))
+		
 
 #['assetId', 'downloadAttribute', 'filetype', 'size', 'downloadLink', 'rawLink']
 defaultcsv = 'ambientCG_downloads_csv.csv'
-
 with open(defaultcsv, newline='') as f:
 	reader = csv.reader(f)
 	assets = list(reader)
 assets.pop(0)
-
-
 listOfDownloadAttributes = []
-listOfTypes = []
 totalSize = 0
-
-for i in assets:
-	if i[2] not in listOfTypes:
-		listOfTypes.append(i[2])
 for i in assets:
 	if i[1] not in listOfDownloadAttributes:
 		listOfDownloadAttributes.append(i[1])
@@ -24,63 +24,66 @@ for i in assets:
 	totalSize += int(i[3])
 
 
-print("Loaded csv file and found:")
-print(len(assets), "assets")
-print("Assets have the following types:")
-print(listOfTypes)
-print("Assets have the following download attributes:")
-print(listOfDownloadAttributes)
-print("All assets combined have a filesize of {0} bytes ({1} gigabytes)".format(totalSize, totalSize/1e+9))
+print("Loaded csv file and found {0} assets".format(len(assets)))
 
-print("Display all asset names? (y/n)")
+print("Would you like to filter assets by a keyword? (not case sensitive) (y/n)")
 while True:
 	userInput = input()
 	if userInput == 'y':
-		for i in assets:
-			print(i[0] +"__"+ i[1] +"__"+ i[2])
+		keyword = input("Enter keyword (NOT CASE SENSITIVE): ")
 		break
 	if userInput == 'n':
+		keyword = None
 		break
 	print("Invalid input")
-
-print("Display information of all assets with a certain attribute?: (y/n)")
+print("Would you like to filter assets by a download attribute? (resolution, filetype, etc) (y/n)")
 while True:
 	userInput = input()
 	if userInput == 'y':
-		s = 0
-		n = 0
-		print("Input download attribute to sort by:")
-		attributeInput = input()
-		for i in assets:
-			if i[1] == attributeInput:
-				n += 1
-				s += int(i[3])
-				print(i[0] +"__"+ i[1] +"__"+ i[2])
-		print("{0} assets found with the attribute {1}, with a total size of {2} bytes ({3} gigabytes)".format(n, attributeInput, s, s/1e+9))
+		print("Found the following download attributes in the csv file:")
+		print(listOfDownloadAttributes)
+		print("Substance files have the download attribute '', just press enter if you want to filter for substance files")
+		inputValid = False
+		while inputValid != True:
+			attributeFilter = input("Enter download attribute (CASE SENSITIVE): ")
+			if attributeFilter in listOfDownloadAttributes:
+				inputValid = True
+			else:
+				print("Invalid download attribute")
 		break
 	if userInput == 'n':
+		attributeFilter = None
 		break
 	print("Invalid input")
 
 
+filteredAssets = getAssetsByFilters(assets, [keyword, attributeFilter])
+filteredAssets.sort()
+filteredTotalSize = 0
+for i in filteredAssets:
+	filteredTotalSize += int(i[3])
+	
+print("found {0} assets that match the filters, with a combined size of {1} bytes ({2} gigabytes)".format(len(filteredAssets), filteredTotalSize, filteredTotalSize/1e+9))
 
-
-print("Display information of all assets with a certain keyword in assetId? (not case sensitive): (y/n)")
+print("Display asset names? (y/n)")
 while True:
 	userInput = input()
 	if userInput == 'y':
-		s = 0
-		n = 0
-		print("Enter keyword:")
-		keyword = input()
-		for i in assets:
-			if keyword.upper() in i[0].upper():
-				n += 1
-				s += int(i[3])
-				print(i[0] +"__"+ i[1] +"__"+ i[2])
-		print("{0} assets found with the keyword {1}, with a total size of {2} bytes ({3} gigabytes)".format(n, keyword, s, s/1e+9))
+		for i in filteredAssets:
+			print(i[0]+"_"+i[1])
 		break
 	if userInput == 'n':
 		break
 	print("Invalid input")
 	
+	
+print("Would you like to download these assets? (y/n)")
+while True:
+	userInput = input()
+	if userInput == 'y':
+		download(filteredAssets)
+		break
+	if userInput == 'n':
+		break
+	print("Invalid input")
+
